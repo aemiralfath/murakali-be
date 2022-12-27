b96c33157a0a7992d7d4e2640b6ca0fd1ff90099
Julius Beckham <julius.beckham@shopee.com>
2022-12-27 09:54:36 +0700

    chore(recommend product list): change name variable consistent

--- internal/model/shop.go
+++ internal/model/shop.go
@@ -13,7 +13,7 @@ type Shop struct {
 	Name         string       `json:"name" db:"name" binding:"omitempty"`
 	TotalProduct int          `json:"total_product" db:"total_product" binding:"omitempty"`
 	TotalRating  float64      `json:"total_rating" db:"total_rating" binding:"omitempty"`
-	RatingAvg    float64      `json:"rating_avg" db:"rating_avg" binding:"omitempty"`
+	RatingAVG    float64      `json:"rating_avg" db:"rating_avg" binding:"omitempty"`
 	CreatedAt    time.Time    `json:"created_at" db:"created_at" binding:"omitempty"`
 	UpdatedAt    sql.NullTime `json:"updated_at" db:"updated_at" binding:"omitempty"`
 	DeletedAt    sql.NullTime `json:"deleted_at" db:"deleted_at" binding:"omitempty"`
