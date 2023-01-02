8c07d668f16f689ea3c4f5e9fbac82f1812c2387
Julius Beckham <julius.beckham@shopee.com>
2023-01-02 10:56:51 +0700

    fix(recommended product list): fixed add product id on recommended product list

--- internal/module/product/delivery/body/recommended_product.go
+++ internal/module/product/delivery/body/recommended_product.go
@@ -1,5 +1,7 @@
 package body
 
+import "github.com/google/uuid"
+
 type RecommendedProductRequest struct {
 }
 
@@ -9,20 +11,21 @@ type RecommendedProductResponse struct {
 }
 
 type Products struct {
-	Title                     string   `json:"title" db:"title"`
-	UnitSold                  int64    `json:"unit_sold" db:"unit_sold"`
-	RatingAVG                 float64  `json:"rating_avg" db:"rating_avg"`
-	ThumbnailURL              string   `json:"thumbnail_url" db:"thumbnail_url"`
-	MinPrice                  float64  `json:"min_price" db:"min_price"`
-	MaxPrice                  float64  `json:"max_price" db:"max_price"`
-	SubPrice                  float64  `json:"sub_price" db:"sub_price"`
-	PromoDiscountPercentage   *float64 `json:"promo_discount_percentage" db:"promo_discount_percentage"`
-	PromoDiscountFixPrice     *float64 `json:"promo_discount_fix_price" db:"promo_discount_fix_price"`
-	PromoMinProductPrice      *float64 `json:"promo_min_product_price" db:"promo_min_product_price"`
-	PromoMaxDiscountPrice     *float64 `json:"promo_max_discount_price" db:"promo_max_discount_price"`
-	ResultDiscount            *float64 `json:"result_discount" db:"result_discount"`
-	VoucherDiscountPercentage *float64 `json:"voucher_discount_percentage" db:"voucher_discount_percentage"`
-	VoucherDiscountFixPrice   *float64 `json:"voucher_discount_fix_price" db:"voucher_discount_fix_price"`
-	ShopName                  string   `json:"shop_name" db:"shop_name"`
-	CategoryName              string   `json:"category_name" db:"category_name"`
+	ID                        uuid.UUID `json:"id" db:"id"`
+	Title                     string    `json:"title" db:"title"`
+	UnitSold                  int64     `json:"unit_sold" db:"unit_sold"`
+	RatingAVG                 float64   `json:"rating_avg" db:"rating_avg"`
+	ThumbnailURL              string    `json:"thumbnail_url" db:"thumbnail_url"`
+	MinPrice                  float64   `json:"min_price" db:"min_price"`
+	MaxPrice                  float64   `json:"max_price" db:"max_price"`
+	SubPrice                  float64   `json:"sub_price" db:"sub_price"`
+	PromoDiscountPercentage   *float64  `json:"promo_discount_percentage" db:"promo_discount_percentage"`
+	PromoDiscountFixPrice     *float64  `json:"promo_discount_fix_price" db:"promo_discount_fix_price"`
+	PromoMinProductPrice      *float64  `json:"promo_min_product_price" db:"promo_min_product_price"`
+	PromoMaxDiscountPrice     *float64  `json:"promo_max_discount_price" db:"promo_max_discount_price"`
+	ResultDiscount            *float64  `json:"result_discount" db:"result_discount"`
+	VoucherDiscountPercentage *float64  `json:"voucher_discount_percentage" db:"voucher_discount_percentage"`
+	VoucherDiscountFixPrice   *float64  `json:"voucher_discount_fix_price" db:"voucher_discount_fix_price"`
+	ShopName                  string    `json:"shop_name" db:"shop_name"`
+	CategoryName              string    `json:"category_name" db:"category_name"`
 }
