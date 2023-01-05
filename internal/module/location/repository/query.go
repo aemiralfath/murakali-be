4674c69810b98cd795ddaa75eb781254191970dd
Ahmad Emir Alfatah <ahmad.alfatah@seamoney.com>
2023-01-05 14:18:14 +0700

    feat(cancel payment): cancel payment done

--- internal/module/location/repository/query.go
+++ internal/module/location/repository/query.go
@@ -1,7 +1,7 @@
 package repository
 
 const (
-	GetCourierByID               = `SELECT "id", "name", "code", "service", "description", "created_at", "updated_at", "deleted_at" FROM "courier" WHERE "id" = $1`
-	GetProductCourierWhitelistID = `SELECT "courier_id" FROM "product_courier_whitelist" WHERE "product_id" = $1`
-	GetShopCourierID             = `SELECT "courier_id" FROM "shop_courier" WHERE "shop_id" = $1`
+	GetCourierByID               = `SELECT "id", "name", "code", "service", "description", "created_at", "updated_at" FROM "courier" WHERE "id" = $1 AND "deleted_at" IS NULL`
+	GetProductCourierWhitelistID = `SELECT "courier_id" FROM "product_courier_whitelist" WHERE "product_id" = $1 AND "deleted_at" IS NULL`
+	GetShopCourierID             = `SELECT "courier_id" FROM "shop_courier" WHERE "shop_id" = $1 AND "deleted_at" IS NULL`
 )
